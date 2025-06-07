70919ed1145a4515a3e08f33ea922b77bb688d76
Stella <wstellawambui@gmail.com>
2025-06-07 20:52:40 +0300

    Homepage

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["./*.html"], // This is enough if all HTML is in root
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
