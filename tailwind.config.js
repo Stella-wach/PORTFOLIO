12e201732a9f037ca0d6199965a9c2ebdc884b26
Stella <wstellawambui@gmail.com>
2025-09-10 23:16:27 +0300

    changed tailwind cdn link to the inbuild tailwind link

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./script.js",
-    "./users/**/*.php",
-    "./*.html",
-    "./*.php"
-  ],
+  content: ["./*.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        display: ['Space Grotesk', 'sans-serif'],
+      },
+      animation: {
+        'spin-slow': 'spin 20s linear infinite',
+      },
+    },
   },
   plugins: [],
 }
